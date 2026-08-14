1551820318586d6edddac8cb848a4f3a500d5a72
Gem Agent <auctionmarketheory@github.com>
2026-08-15 01:32:30 +0700

    fix: update launcher path to /storage/roms/ports/

--- launchers/MicTest.sh
+++ launchers/MicTest.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # MicTest.sh - Launcher for R36S (EmuELEC/ArkOS ports)
-# Place in: /storage/EASYROMS/ports/
+# Place in: /storage/roms/ports/
 
-DIR="$(dirname "$0")"
-cd "$DIR"
 export HOME=/storage
 export LD_LIBRARY_PATH=/usr/lib32:/usr/lib:$LD_LIBRARY_PATH
+
+# Change to the app directory inside ports
+cd /storage/roms/ports/App_MicTest
 
 # Log output for debugging
 ./App_MicTest > /tmp/MicTest.log 2>&1
